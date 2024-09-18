38dc94d19be50697e4e83be73f85a0542ad8e0ed
AnandKosuri <anandprince66@gmail.com>
2024-09-18 16:30:07 +0530

    Add files via upload
    Automation code

---
+++ E2EIntegration/E2ESeleniumJenkins/src/testcases/TC_003.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	@Test
+	public void testcase3(){
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello");
+		driver.close();
+	}
+
+}
